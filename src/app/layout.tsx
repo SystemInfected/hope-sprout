d51681f557664554fdd8dea2a4ae8bdf4bb573e9
Sebastian Widin <contact@sebastianwidin.se>
2023-07-14 23:10:49 +0200

    prettified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,29 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import Navigation from './layout/Navigation'
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['200', '500', '600', '700'] })
+const poppins = Poppins({
+	subsets: ['latin'],
+	weight: ['200', '500', '600', '700'],
+})
 
 export const metadata: Metadata = {
-  title: 'HopeSprout',
-  description: 'HopeSprout',
+	title: 'HopeSprout',
+	description: 'HopeSprout',
 }
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<body className={poppins.className}>
+				<Navigation />
+				{children}
+			</body>
+		</html>
+	)
 }
